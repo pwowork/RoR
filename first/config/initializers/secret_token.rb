a3ed814f6c4b9cf591500bc340eca5e97ea00e4d
pwowork <pwowork@gmail.com>
2011-12-24 22:41:26 +0200

    init first project

---
+++ first/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+First::Application.config.secret_token = 'aeb9d2181fbffd781abf7863d459ef35e372c3f6d8c3a28fbe8d7803555cb305df2dee5fed4a71fa6a8f2962e02eaed34896a735750c345067c9714c5e0ddc33'
